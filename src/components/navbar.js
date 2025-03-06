452b6d1c642fd74efa138ab74926127cca30a8fd
Qeksy <patryk.miazek211@gmail.com>
2025-03-06 10:28:35 +0100

    Test Navbar2

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,48 @@
-import React from "react";
-import { Navbar, Conteiner, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Navbar,
+  Container,
+  Button,
+  Collapse,
+  Card,
+  Nav,
+} from "react-bootstrap";
 import { List } from "react-bootstrap-icons";
 
 const NavigationBar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Navbar bg="dark" variant="dark">
-      <Conteiner>
-        <Navbar.Brand href="/">MyApp</Navbar.Brand>
+    <div style={{ position: "relative" }}>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Movie Rulette</Navbar.Brand>
 
-        <Button variant="outline-light">
-          <List size={30} />
-        </Button>
-      </Conteiner>
-    </Navbar>
+          <Collapse in={open} dimension="width">
+            <div>
+              <Card body style={{ width: "400px" }}>
+                <Nav>
+                  <Nav.Link href="/" style={{ color: "red" }}>
+                    Main
+                  </Nav.Link>
+                  <Nav.Link href="/views/app" style={{ color: "red" }}>
+                    Main
+                  </Nav.Link>
+                </Nav>
+              </Card>
+            </div>
+          </Collapse>
+
+          <Button
+            variant="outline-light"
+            onClick={() => setOpen(!open)}
+            aria-controls="-collapse-text"
+            aria-expanded={open}
+          >
+            <List size={30} />
+          </Button>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
