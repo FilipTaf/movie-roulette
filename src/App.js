f47108e536094dfd5f88093d7b77d2840626b0cd
MikoLajWy <mikolajwyskocki@gmail.com>
2025-03-07 08:56:45 +0100

    Deletion of test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Test from "./Test";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <Test />
 
         <a
           className="App-link"
