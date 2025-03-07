ab948d7887ebe7f18df497634a01821fefb37c4b
FilipTaf <filip.taf@interia.pl>
2025-03-07 08:59:23 +0100

    solved some merge shenanigans

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import History from "./views/history";
 import Navibar from "./components/navbar";
+import Mainbody from "./components/restWebpage";
+import MyNavbar from "./components/navbar";
 
 function App() {
   return (
+    <>
     <Router>
-      <Navibar />
+      <MyNavbar />
       <Routes>
         <Route path="/src/views/history" element={<History />} />
       </Routes>
     </Router>
+    <Mainbody />
+    </>
   );
 }
 
