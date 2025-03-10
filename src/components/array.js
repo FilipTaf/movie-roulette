1abbc16f942a3f98fdcac52f3b91a7ca0a4e6b0f
Qeksy <patryk.miazek211@gmail.com>
2025-03-10 12:42:15 +0100

    History + addFav

--- src/components/array.js
+++ src/components/array.js
@@ -1,9 +1,21 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleMovie } from "../store/movieReducer";
+import { Modal, Button } from "react-bootstrap";
 
 const MovieList = ({ query = "", isSearchClicked }) => {
   const movieList = useSelector((state) => state.movies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("Renderowanie komponentu", movieList);
+  }, [movieList]);
+
+  const handleToggle = (title) => {
+    console.log("Kliknieto przycisk dla ", title);
+    dispatch(toggleMovie(title));
+  };
 
   const filteredMovies = useMemo(() => {
     if (!isSearchClicked || !query || !isSearchClicked != "") {
@@ -19,29 +31,57 @@ const MovieList = ({ query = "", isSearchClicked }) => {
     () => filteredMovies.slice(0, 30),
     [filteredMovies]
   );
-
-  console.log(moviesToDisplay);
+  const [show, setShow] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const Close = () => setShow(false);
+  const Show = (movie) => {
+    setSelectedMovie(movie);
+    setShow(true);
+  };
   return (
-    <Table bordered hover>
-      <thead>
-        <tr>
-          <th>Tytuł</th>
-          <th>Gatunek</th>
-          <th>Reżyser</th>
-          <th>Ocena</th>
-        </tr>
-      </thead>
-      <tbody>
-        {moviesToDisplay.map((movie, index) => (
-          <tr key={index}>
-            <td>{movie.title}</td>
-            <td>{movie.genre}</td>
-            <td>{movie.director}</td>
-            <td>{movie.rating}</td>
+    <>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th>Tytuł</th>
+            <th>Gatunek</th>
+            <th>Reżyser</th>
+            <th>Ocena</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {moviesToDisplay.map((movie, index) => (
+            <tr key={index}>
+              <td onClick={() => Show(movie)}>{movie.title}</td>
+              <td>{movie.genre}</td>
+              <td>{movie.director}</td>
+              <td>{movie.rating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Modal show={show} onHide={Close} backdrop="static" keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedMovie ? selectedMovie.title : ""}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Reżyser : {selectedMovie ? selectedMovie.director : ""}
+          Opis : {selectedMovie ? selectedMovie.description : ""}
+          Gatunek : {selectedMovie ? selectedMovie.genre : ""}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={Close}>
+            Zamknij
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => handleToggle(selectedMovie.title)}
+          >
+            Dodaj do ulubionych
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
