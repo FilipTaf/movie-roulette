0fe44d35529d99fe5fa46e7d76bab5297ba4579b
Qeksy <patryk.miazek211@gmail.com>
2025-03-12 12:13:36 +0100

    View

---
+++ src/components/useFavSe.js
@@ -0,0 +1,22 @@
+import React, { useState, useCallback } from "react";
+import Search from "./search";
+import MovieList from "./favArray";
+
+const General = () => {
+  const [query, setQuery] = useState("");
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+
+  const handleSearch = useCallback((query) => {
+    setQuery(query);
+    setIsSearchClicked(true);
+  }, []);
+
+  return (
+    <div className="container mt-3">
+      <Search onSearch={handleSearch} />
+      <MovieList query={query} isSearchClicked={isSearchClicked} />
+    </div>
+  );
+};
+
+export default General;
