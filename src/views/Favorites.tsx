52336fac2d55967b3c6be0618a3a06aad2d4a879
MikoLajWy <mikolajwyskocki@gmail.com>
2025-03-13 14:56:26 +0100

    Added animated logo

--- src/views/Favorites.tsx
+++ src/views/Favorites.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import General from "../components/ToFavorite/Use";
-import { useAppSelector } from "../store/hooks";
+import MovieList from "../components/favArray";
 
 const Favorites = () => {
-  const isAuth = useAppSelector((state) => state.auth.isAuth)
   return (
     <div>
-      {isAuth ? (<General />) : (<h1>You must be logged in</h1>)}
+      <MovieList isSearchClicked={undefined} />
     </div>
   );
 };
