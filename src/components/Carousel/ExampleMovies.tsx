51b49d8d44dddb151e391fb7f6086cee1b21e7d3
MikoLajWy <mikolajwyskocki@gmail.com>
2025-03-18 11:46:13 +0100

    Reduced carousel size

--- src/components/Carousel/ExampleMovies.tsx
+++ src/components/Carousel/ExampleMovies.tsx
@@ -5,16 +5,7 @@ import { useAppSelector } from "../../store/hooks";
 
 const ExampleMovies = () => {
   const movieList = useAppSelector((state) => state.movies);
-  interface Movie {
-    id: number;
-    title: string;
-    description: string;
-    director: string;
-    release: string;
-    genre: string;
-    rating: string;
-    favorite: boolean;
-  }
+
   const rand = Math.floor(Math.random() * 50);
 
   const chosenMovies: Array<number> = [];
@@ -24,41 +15,43 @@ const ExampleMovies = () => {
 
   return (
     <>
-      <h1 style={{ marginTop: "150px", color: "white" }} id="Example">
+      <h1 style={{ marginTop: "100px", color: "white" }} id="Example">
         Example Movies
       </h1>
-      <Carousel>
-        <Carousel.Item
-          style={{
-            backgroundImage: "url(" + movieList[chosenMovies[0]].img + ")",
-          }}
-        >
-          <Carousel.Caption>
-            <h3>{movieList[chosenMovies[0]].title}</h3>
-            <p>{movieList[chosenMovies[0]].description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item
-          style={{
-            backgroundImage: "url(" + movieList[chosenMovies[1]].img + ")",
-          }}
-        >
-          <Carousel.Caption>
-            <h3>{movieList[chosenMovies[1]].title}</h3>
-            <p>{movieList[chosenMovies[1]].description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item
-          style={{
-            backgroundImage: "url(" + movieList[chosenMovies[2]].img + ")",
-          }}
-        >
-          <Carousel.Caption>
-            <h3>{movieList[chosenMovies[2]].title}</h3>
-            <p>{movieList[chosenMovies[2]].description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <center>
+        <Carousel>
+          <Carousel.Item
+            style={{
+              backgroundImage: "url(" + movieList[chosenMovies[0]].img + ")",
+            }}
+          >
+            <Carousel.Caption>
+              <h3>{movieList[chosenMovies[0]].title}</h3>
+              <p>{movieList[chosenMovies[0]].description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item
+            style={{
+              backgroundImage: "url(" + movieList[chosenMovies[1]].img + ")",
+            }}
+          >
+            <Carousel.Caption>
+              <h3>{movieList[chosenMovies[1]].title}</h3>
+              <p>{movieList[chosenMovies[1]].description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item
+            style={{
+              backgroundImage: "url(" + movieList[chosenMovies[2]].img + ")",
+            }}
+          >
+            <Carousel.Caption>
+              <h3>{movieList[chosenMovies[2]].title}</h3>
+              <p>{movieList[chosenMovies[2]].description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </center>
     </>
   );
 };
