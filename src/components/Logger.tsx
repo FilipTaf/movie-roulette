f9fad7100c93960a34321ed5cbbd2df8448b87a7
MikoLajWy <mikolajwyskocki@gmail.com>
2025-03-19 14:18:37 +0100

    Fixed the error after merge

--- src/components/Logger.tsx
+++ src/components/Logger.tsx
@@ -44,7 +44,7 @@ const Login = () => {
           setIsLoading(false);
           setIsSelectRegister(false);
           setIsSelectLogin(true);
-          dispatch(login());
+          dispatch(login(role));
           navigate("../Roulette");
         }, 1000);
       } else {
