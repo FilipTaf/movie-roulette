9465743e3b9e92fd50227b4a0a433801415a2685
FilipTaf <filip.taf@interia.pl>
2025-03-27 13:38:57 +0100

    fixed the website

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
 import React from "react";
 import App from "../App";
+import ReactDOM from "react-dom/client";
+import "../index.css";
+import store, { persistor } from "../store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  );
+    root.render(
+      <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+      ,
+    </React.StrictMode>
+    )
+  )
 }
+
+
